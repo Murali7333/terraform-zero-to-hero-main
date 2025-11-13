a876f45075586ace174de9fa0b257566147675b4
Muralidhar Reddy <muralireddy.varikuti@gmail.com>
2025-11-13 09:01:18 +0530

    added the code

---
+++ providers_multicloud.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+    features {}
+   subscription_id ="f68bb8db-d1d5-420c-9769-c743c87a2895"
+}
+
+provider "aws" {
+    region ="apsoutheast=2"
+}
---
+++ resourcegroup.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "myresourcegroup" {
+
+    name="rg-useast-app-01"
+    location ="East US"
+}
+
+
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "test bucket"
+
+}
